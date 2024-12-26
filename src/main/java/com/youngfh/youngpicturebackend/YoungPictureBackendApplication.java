9ea65667c53a15afc2a57b2f4f5ac491dbcc3efa
WenBaCha <fuhaoyang1@qq.com>
2024-12-26 21:29:31 +0800

    Project init 00

---
+++ src/main/java/com/youngfh/youngpicturebackend/YoungPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.youngfh.youngpicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoungPictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youngfh/youngpicturebackend/YoungPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngfh.youngpicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
